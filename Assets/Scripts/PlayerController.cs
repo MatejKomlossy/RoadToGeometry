6748643a32af22b70b76bd10169c29701cfe7d63
Aru <jan.boborova@gmail.com>
2022-11-11 15:04:02 +0100

    car steering, side speed according to head rotation

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
@@ -10,11 +11,6 @@ public class PlayerController : MonoBehaviour
 
     public float playerMinX = 7.6f;
     public float playerMaxX = 11.3f;
-
-    private const float MinAnglesLeft = 10;
-    private const float MaxAnglesLeft = 70;
-    private const float MinAnglesRight = 190;
-    private const float MaxAnglesRight = 350;
 
     private float decelerationSpeed = 0.2f;
 
@@ -35,25 +31,27 @@ public class PlayerController : MonoBehaviour
         
         Quaternion headRotation = Camera.main.transform.rotation;
         Vector3 currentEulerAngles = headRotation.eulerAngles;
-        
-        float sideMovementSpeed = forwardMovementSpeed / 5;
-        
-        if (currentEulerAngles.z > MinAnglesRight && currentEulerAngles.z < MaxAnglesRight)
-        {
-            if (transform.position.x < playerMaxX)
-            {
-                transform.position -= Vector3.left * (Time.deltaTime * sideMovementSpeed); //to the right, comrades
-            }
-        }
-        else if (currentEulerAngles.z > MinAnglesLeft && currentEulerAngles.z < MaxAnglesLeft)
+
+        if (currentEulerAngles.z > 0 && currentEulerAngles.z < 180)
         {
             if (transform.position.x > playerMinX)
             {
-                transform.position += Vector3.left * (Time.deltaTime * sideMovementSpeed); //to the left, comrades
+                float angle = Mathf.Min(currentEulerAngles.z, 90); // if more than 90, use 90 (max left speed)
+                float sideMovementSpeed = forwardMovementSpeed * Mathf.Sin(Mathf.Deg2Rad * angle);
+                transform.position += Vector3.left * (Time.deltaTime * sideMovementSpeed);
             }
         }
-        
-        
+        else if (currentEulerAngles.z > 180 && currentEulerAngles.z < 360)
+        {
+            if (transform.position.x < playerMaxX)
+            {
+                float angle = Mathf.Max(currentEulerAngles.z, 270); // if more less than 270, use 270 (max right speed)
+                float sideMovementSpeed = forwardMovementSpeed * Mathf.Sin(Mathf.Deg2Rad * angle);
+                transform.position += Vector3.left * (Time.deltaTime * sideMovementSpeed);
+            }
+        }
+
+
         // PC
         if (Input.GetKey(KeyCode.W))
         {
